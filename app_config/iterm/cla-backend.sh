73f8eef7ea856db5d50c489e1f68e71b8b43a2eb
Dom Smith <dom@teneightfive.com>
2014-09-25 15:09:31 +0100

    Run backend in testing mode locally

--- app_config/iterm/cla-backend.sh
+++ app_config/iterm/cla-backend.sh
@@ -4,4 +4,4 @@ git pull
 source env/bin/activate
 pip install -r requirements/local.txt
 ./manage.py migrate
-./manage.py runserver 0.0.0.0:8000
+./manage.py runserver 0.0.0.0:8000 --settings cla_backend.settings.testing
