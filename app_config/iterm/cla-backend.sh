bd784a28ae8c706bab0e3d72b21682f035bbf08b
Dom Smith <dom@teneightfive.com>
2015-03-03 11:39:55 +0000

    Update CLA initial load command to include fixtures

--- app_config/iterm/cla-backend.sh
+++ app_config/iterm/cla-backend.sh
@@ -4,4 +4,4 @@ git pull
 source env/bin/activate
 pip install -r requirements/local.txt
 ./manage.py migrate
-./manage.py runserver 0.0.0.0:8000 --settings cla_backend.settings.testing
+./manage.py testserver initial_groups.json kb_from_knowledgebase.json initial_category.json test_provider.json initial_mattertype.json test_auth_clients.json initial_media_codes.json test_rotas.json test_casearchived.json test_providercases.json test_provider_allocations.json  --noinput --settings cla_backend.settings.testing
