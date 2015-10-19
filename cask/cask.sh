02e2d8f9bee448d06fafef3efcccd28f5cca91b8
Dom Smith <dom@teneightfive.com>
2015-10-19 17:25:39 +0100

    Update app list installed via cask

--- cask/cask.sh
+++ cask/cask.sh
@@ -1,45 +1,43 @@
 # Install native apps
-brew tap phinze/homebrew-cask
-brew install brew-cask
+brew install caskroom/cask/brew-cask
 
 function installcask() {
   brew cask install "${@}" 2> /dev/null
 }
 
+installcask alfred
+installcask atom
+installcask charles
+installcask diffmerge
+installcask dropbox
+installcask firefox
+installcask flux
+installcask gopro-studio
 installcask google-chrome
 installcask google-drive
-installcask google-chrome-canary
-installcask imagealpha
+installcask handbrake
 installcask imageoptim
 installcask iterm2
-installcask sublime-text # 3???
-installcask the-unarchiver
-installcask ukelele
+installcask licecap
+installcask opera
+installcask pycharm
+installcask sequel-pro
+installcask sketch
+installcask skype
+installcask sourcetree
+installcask spotify
 installcask virtualbox
 installcask vlc
 
-installcask alfred
-installcask dash
-installcask dash
-installcask charles
-installcask firefox
-installcask evernote
-installcask hipchat
-installcask sourcetree
-installcask spotify
-installcask sequel-pro
-installcask skype
-installcask brackets
-installcask onepassword
-installcask transmit
-installcask filezilla
-installcask mamp
-installcask paralells
-installcask opera
+## Can be synced via app store?
+#installcask 1password
+#installcask caffeine
+#installcask dash
+#installcask evernote
+#installcask moom
+#installcask slack
+#installcask the-unarchiver
+#installcask transmit
 
-## Other potentials to check?
-# Moom
-# TweetDeck
-# Mamp pro
-# MS Office
-# Safari
+## Manual
+#tweetdeck
