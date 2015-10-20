ce1e00d69712ad8618a2c7ae994cdb2adbe88ffd
Dom Smith <dom@teneightfive.com>
2015-10-20 17:29:38 +0100

    Restructure app config

--- app_config/iterm/cla-backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-git pull
-source env/bin/activate
-pip install -r requirements/local.txt
-./manage.py migrate
-./manage.py testserver initial_groups.json kb_from_knowledgebase.json initial_category.json test_provider.json initial_mattertype.json test_auth_clients.json initial_media_codes.json test_rotas.json test_casearchived.json test_providercases.json test_provider_allocations.json  --noinput --settings cla_backend.settings.testing
--- app_config/iterm/cla-callmeback.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-
-git pull
-source env/bin/activate
-pip install -r requirements/local.txt
-./manage.py runserver_plus 0.0.0.0:8006
