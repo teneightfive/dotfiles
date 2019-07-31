03a811c93696b576563887dad000a6318484864c
Dom Smith <dom@teneightfive.com>
2019-07-31 11:00:04 +0100

    Update default npm packages

--- scripts/npm_bundles.rb
+++ scripts/npm_bundles.rb
@@ -4,24 +4,8 @@ puts "Running Node version: #{`node --version`}"
 npms = {
   # module name => module command
   # leave command blank if they are the same
-  "apiconnect" => "apic",
-  "azure-cli" => "azure",
-  "casperjs" => "",
-  "dalek-cli" => "dalek",
-  "express" => "",
-  "gh" => "",
-  "grunt-cli" => "grunt",
-  "gulp" => "",
-  "handlebars" => "",
-  "html2jade" => "",
-  "hubot" => "",
-  "jscs" => "",
-  "jshint" => "",
-  "mocha" => "",
-  "nodemon" => "",
-  "phantomjs" => "",
-  "sails" => "",
-  "yo" => "",
+  "ndb" => "",
+  "now" => "",
 }
 
 npms.each do |mod, command|
