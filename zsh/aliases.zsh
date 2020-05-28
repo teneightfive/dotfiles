d97822009d8ea2154903a713dddfd26a71bff2d3
Dom Smith <dom@teneightfive.com>
2020-05-28 14:11:23 +0100

    Add useful git, ruby and npm aliases to zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,20 @@ function gcob() {
 function gcl() {
   git clone "$@" && cd `echo $_ | sed -n -e 's/^.*\/\([^.]*\)\(.git\)*/\1/p'`
 }
+
+# git
+alias gs="git status --short --branch"
+
+# ruby
+alias be="bundle exec"
+alias bi="bundle install"
+
+# npm
+alias n="node"
+alias ni="npm install"
+alias nis="npm install --save"
+alias nisd="npm install --save-dev"
+alias nui="npm uninstall"
+alias nr="npm run"
+alias ns="npm start"
+alias ng="npm list -g --depth=0 2>/dev/null"
